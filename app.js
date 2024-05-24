792d06eb43ffef44517b74f11cc1caac85149a71
Daniel <dhonaruto@gmail.com>
2024-05-24 02:07:54 -0400

    complete refactoring and added documentation

--- app.js
+++ app.js
@@ -1,30 +1,26 @@
-if (process.env.NODE_ENV !== "production") {
+// if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
-}
+// }
 
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
-const ejsMate = require("ejs-mate");
 const session = require("express-session");
 const flash = require("connect-flash");
-const ExpressError = require("./utils/ExpressError");
-const methodOverride = require("method-override");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
+const methodOverride = require("method-override");
+const ExpressError = require("./utils/ExpressError");
 const User = require("./models/user");
-const mongoSanitize = require("express-mongo-sanitize");
-const dbUrl = "mongodb://127.0.0.1:27017/campsite";
 const MongoStore = require('connect-mongo');
+const ejsMate = require("ejs-mate");
+const mongoSanitize = require("express-mongo-sanitize");
 
-// "mongodb://127.0.0.1:27017/campsite"
+const app = express();
 
-// Express Router
-const userRoutes = require("./routes/users");
-const campgroundRoutes = require("./routes/campgrounds");
-const reviewRoutes = require("./routes/reviews");
+const dbUrl = "mongodb://127.0.0.1:27017/campsite";
 
-// Connect Database with Mongoose
+// Connect MongoDB with Mongoose
 mongoose
   .connect(dbUrl)
   .then(() => {
@@ -41,24 +37,22 @@ db.once("open", () => {
   console.log("Database connected");
 });
 
-const app = express();
-const port = 3000;
-
-// Middleware (ejs)
+// Middleware configuration
 app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-
-app.use(express.urlencoded({ extended: true })); // Middleware (express parse)
+app.use(express.urlencoded({ extended: true })); // (express parse)
 app.use(methodOverride("_method")); // Override method for PUT and DELETE in form
 app.use(express.static(path.join(__dirname, "public")));
 app.use(mongoSanitize());
 
+// Session configuration
+const secret = process.env.SECRET;
 const store = MongoStore.create({
   mongoUrl: dbUrl,
   touchAfter: 24 * 60 * 60,
   crypto: {
-    secret: "thisshouldbeabettersecret",
+    secret,
   },  
 });
 
@@ -69,26 +63,29 @@ store.on("error", function(e) {
 const sessionConfig = {
   store,
   name: "session",
-  secret: "thisshouldbeabettersecret!",
+  secret,
   resave: false,
   saveUninitialized: true,
   cookie: {
     httpOnly: true,
-    // secure: true,
     expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
     maxAge: 1000 * 60 * 60 * 24 * 7,
   },
 };
+
 app.use(session(sessionConfig));
 app.use(flash());
 
+// Passport configuration
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
+// Store a user in the session
+passport.serializeUser(User.serializeUser()); 
+// Get a user out of the session
+passport.deserializeUser(User.deserializeUser()); 
 
-passport.serializeUser(User.serializeUser()); // Store a user in the session
-passport.deserializeUser(User.deserializeUser()); // Get a user out of the session
-
+// Flash and locals middleware
 // Allow access to 'currentUser', 'success', 'error' in templates
 app.use((req, res, next) => {
   res.locals.currentUser = req.user;
@@ -97,25 +94,28 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use("/", userRoutes);
-app.use("/campgrounds", campgroundRoutes);
-app.use("/campgrounds/:id/reviews", reviewRoutes);
+// Routes
+const indexRoutes = require('./routes/index');
+app.use('/', indexRoutes);
 
+// Home
 app.get("/", (req, res) => {
   res.render("home");
 });
 
-// For every path
+// Error handling
 app.all("*", (req, res, next) => {
   next(new ExpressError("Page Not Found", 404));
 });
 
 app.use((err, req, res, next) => {
   const { statusCode = 500 } = err;
-  if (!err.message) err.message = "Oh no, something went wrong";
+  if (!err.message) err.message = "Something went wrong!";
   res.status(statusCode).render("error", { err });
 });
 
+// Start server
+const port = 3000;
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
